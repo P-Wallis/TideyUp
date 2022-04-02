e6868e023a306842cbea564cd5ed45871c805a15
Jordan Harrison <jcharrison12@gmail.com>
2022-04-02 18:47:40 -0500

    Combines animations with left/right movement, jumping
    Still do not have sprite image flipping when reversing direction, but walking, idle, and jump work

--- CharacterController.cs
+++ CharacterController.cs
@@ -5,30 +5,57 @@ public class CharacterController : KinematicBody2D
 {
     const float gravity = 200.0f;
     const int walkSpeed = 200;
-
+    [Export]
+    public int JumpImpulse =  -400;
     Vector2 velocity;
+    public AnimatedSprite _animatedSprite;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        _animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+    }
 
     public override void _PhysicsProcess(float delta)
     {
-        velocity.y += delta * gravity;
-
+        if (IsOnFloor())
+        {
+            velocity.y = gravity;
+        }
+        else
+        {
+            velocity.y += delta * gravity;
+        }
+		
         if (Input.IsActionPressed("ui_left"))
         {
             velocity.x = -walkSpeed;
+            _animatedSprite.Play("Run");
         }
         else if (Input.IsActionPressed("ui_right"))
         {
             velocity.x = walkSpeed;
+            _animatedSprite.Play("Run");
         }
         else
         {
             velocity.x = 0;
+            _animatedSprite.Play("Idle");
         }
 
-        // We don't need to multiply velocity by delta because "MoveAndSlide" already takes delta time into account.
+        // Jumping.
+        if (IsOnFloor() && Input.IsActionPressed("ui_select"))
+        {
+            velocity.y += JumpImpulse;
+            _animatedSprite.Play("Jump");
+        }
 
-        // The second parameter of "MoveAndSlide" is the normal pointing up.
-        // In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
-        MoveAndSlide(velocity, new Vector2(0, -1));
+        // ...
+    // We don't need to multiply velocity by delta because "MoveAndSlide" already takes delta time into account.
+
+    // The second parameter of "MoveAndSlide" is the normal pointing up.
+    // In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
+    MoveAndSlide(velocity, new Vector2(0, -1));
     }
 }
