ee42bd002deef218f712b515dd0a6c96716db1d8
Peter <p.a.e.wallis@gmail.com>
2022-04-03 01:01:42 -0500

    New Main Scene
    * Moved code to connect scene planks into its own script
    * Made a new house scene
    * Made new main scene with player and house

--- Building/BuildingTest.cs
+++ Building/BuildingTest.cs
@@ -2,36 +2,9 @@ using Godot;
 using System;
 using Random = TideyUp.Utils.Random;
 
-public class BuildingTest : Node2D
+public class BuildingTest : BuildingBase
 {
     PackedScene PlankScene = GD.Load<PackedScene>("res://Building/Plank.tscn");
-
-    public override void _Ready()
-    {
-        base._Ready();
-
-        ConnectPlanksInChildrenRecursively(this);
-    }
-
-    void ConnectPlanksInChildrenRecursively(Node node)
-    {
-        Godot.Collections.Array children = node.GetChildren();
-        Plank plank;
-        Node child;
-        for(int i=0; i<children.Count; i++)
-        {
-            child = (Node)children[i];
-            if(child.HasSignal(Plank.IS_PLANK_SIGNAL))
-            {
-                plank = (Plank)children[i];
-                plank.ConnectToOtherPlanks();
-            }
-            else
-            {
-                ConnectPlanksInChildrenRecursively(child);
-            }
-        }
-    }
 
     public override void _Input(InputEvent @event)
     {
