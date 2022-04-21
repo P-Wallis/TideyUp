26a165b048a870e6f47d953aecb598cf31ee5bb1
Peter <p.a.e.wallis@gmail.com>
2022-04-21 00:56:10 -0500

    Wave and Button Sounds
    * Added wave sound
    * Added button sound to main scene (so it's not cut off when the scene loads)
    * Modified instructions to include up and down arrows for plank movement

--- Scripts/PlayButton.cs
+++ Scripts/PlayButton.cs
@@ -3,13 +3,16 @@ using System;
 
 public class PlayButton : Button
 {
+    private AudioStreamPlayer sound;
     public override void _Ready()
     {
         Connect("pressed", this, nameof(OnPressed));
+        sound = SceneManager._.GetNode<AudioStreamPlayer>("ButtonSFX");
     }
 
     void OnPressed()
     {
+        sound.Play();
         if(SceneManager._!=null)
         {
             SceneManager._.LoadScene(SceneID.Gameplay);
